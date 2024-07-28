38ed1266c90dcee00dc33e60081034549828fe6a
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-07-29 02:16:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from colorCodeTests import test_number_to_pair, test_pair_to_number
+from colorCodeTests import test_number_to_pair, test_pair_to_number, test_print_color_coding_manual
 
 if __name__ == '__main__':
   test_number_to_pair(4, 'White', 'Brown')
@@ -6,4 +6,5 @@ if __name__ == '__main__':
   test_pair_to_number('Black', 'Orange', 12)
   test_pair_to_number('Violet', 'Slate', 25)
   test_pair_to_number('Red', 'Orange', 7)
+  test_print_color_coding_manual()
   print('Done :)')
