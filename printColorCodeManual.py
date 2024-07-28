9be700ce36acf3994dc0510a46f2d3062596ac0b
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-07-29 02:14:51 +0530

    Create printColorCodeManual.py

---
+++ printColorCodeManual.py
@@ -0,0 +1,11 @@
+from constantColorValues import MAJOR_COLORS, MINOR_COLORS
+from ColorCodeFunctions import get_color_from_pair_number
+
+def print_color_coding_manual():
+    manual = "Color Coding Manual\n"
+    manual += "Pair Number | Major Color | Minor Color\n"
+    manual += "------------|-------------|------------\n"
+    for pair_number in range(1, len(MAJOR_COLORS) * len(MINOR_COLORS) + 1):
+        major_color, minor_color = get_color_from_pair_number(pair_number)
+        manual += f'{pair_number:11} | {major_color:12} | {minor_color}\n'
+    return manual
