67e0f64f1e341482c0d883cf735167a17efd08a2
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-07-29 01:52:49 +0530

    Update testColorCode.py

--- testColorCode.py
+++ testColorCode.py
@@ -1,5 +1,3 @@
-import main
-
 def test_number_to_pair(pair_number,
                         expected_major_color, expected_minor_color):
   major_color, minor_color = get_color_from_pair_number(pair_number)
