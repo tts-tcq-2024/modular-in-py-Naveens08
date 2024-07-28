cd41ce36a018a323310912f9a3fca89e253c8479
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-07-29 02:15:39 +0530

    Update colorCodeTests.py

--- colorCodeTests.py
+++ colorCodeTests.py
@@ -1,4 +1,5 @@
 from ColorCodeFunctions import get_color_from_pair_number, get_pair_number_from_color
+from printColorCodeManual import print_color_coding_manual
 
 def test_number_to_pair(pair_number,
                         expected_major_color, expected_minor_color):
@@ -10,3 +11,7 @@ def test_number_to_pair(pair_number,
 def test_pair_to_number(major_color, minor_color, expected_pair_number):
   pair_number = get_pair_number_from_color(major_color, minor_color)
   assert(pair_number == expected_pair_number)
+
+def test_print_color_coding_manual():
+    manual = print_color_coding_manual()
+    print(manual)
