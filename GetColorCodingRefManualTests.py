74fbee2220481735e4588247a9d45324567704e3
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-08-05 09:37:27 +0530

    Update GetColorCodingRefManualTests.py

--- GetColorCodingRefManualTests.py
+++ GetColorCodingRefManualTests.py
@@ -1,33 +1,7 @@
 from GetColorCodeRefManual import get_color_coding_ref_manual
+from getColorCodingExpectedManual import get_expected_color_coding_manual
 
 def test_get_color_coding_ref_manual():
-    expected_manual = ("Color Coding Reference Manual\n"
-        "Pair Number | Major Color | Minor Color\n"
-        "------------|-------------|------------\n"
-        "          1 | White       | Blue\n"
-        "          2 | White       | Orange\n"
-        "          3 | White       | Green\n"
-        "          4 | White       | Brown\n"
-        "          5 | White       | Slate\n"
-        "          6 | Red         | Blue\n"
-        "          7 | Red         | Orange\n"
-        "          8 | Red         | Green\n"
-        "          9 | Red         | Brown\n"
-        "         10 | Red         | Slate\n"
-        "         11 | Black       | Blue\n"
-        "         12 | Black       | Orange\n"
-        "         13 | Black       | Green\n"
-        "         14 | Black       | Brown\n"
-        "         15 | Black       | Slate\n"
-        "         16 | Yellow      | Blue\n"
-        "         17 | Yellow      | Orange\n"
-        "         18 | Yellow      | Green\n"
-        "         19 | Yellow      | Brown\n"
-        "         20 | Yellow      | Slate\n"
-        "         21 | Violet      | Blue\n"
-        "         22 | Violet      | Orange\n"
-        "         23 | Violet      | Green\n"
-        "         24 | Violet      | Brown\n"
-        "         25 | Violet      | Slate\n")
+    expected_manual = get_expected_color_coding_manual()
     actual_manual = get_color_coding_ref_manual()
     assert (actual_manual == expected_manual)
