1624e68cf06978f5eee106760dc968838501dd21
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-08-05 09:38:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from colorCodeTests import test_number_to_pair, test_pair_to_number, test_print_color_coding_manual
+from ColorCodeFunctionsTests import test_number_to_pair, test_pair_to_number, test_print_color_coding_manual
 from GetColorCodingRefManualTests import test_get_color_coding_ref_manual
 
 if __name__ == '__main__':
