a4841aa32aeeef17ee47fe58b834273226965b48
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-08-05 09:43:22 +0530

    Update GetColorCodingRefManualTests.py

--- GetColorCodingRefManualTests.py
+++ GetColorCodingRefManualTests.py
@@ -1,7 +1,9 @@
 from GetColorCodeRefManual import get_color_coding_ref_manual
-from getColorCodingExpectedManual import get_expected_color_coding_manual
+from GetColorCodeExpectedManual import get_expected_color_coding_manual
 
 def test_get_color_coding_ref_manual():
     expected_manual = get_expected_color_coding_manual()
     actual_manual = get_color_coding_ref_manual()
+    print(expected_manual)
+    print(actual_manual)
     assert (actual_manual == expected_manual)
