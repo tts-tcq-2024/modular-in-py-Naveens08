a5ad18d3b5563e76753b4c2a19c1336d126dfdb9
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-08-05 09:24:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
   test_pair_to_number('Black', 'Orange', 12)
   test_pair_to_number('Violet', 'Slate', 25)
   test_pair_to_number('Red', 'Orange', 7)
-  test_print_color_coding_manual()
+  test_get_color_coding_ref_manual()
   print('Done :)')
