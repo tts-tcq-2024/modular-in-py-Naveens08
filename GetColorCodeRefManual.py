2c830551534663aa410c9788cf5de8fc526b88ad
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-08-05 09:51:36 +0530

    Update GetColorCodeRefManual.py

--- GetColorCodeRefManual.py
+++ GetColorCodeRefManual.py
@@ -7,5 +7,5 @@ def get_color_coding_ref_manual():
     manual += "------------|-------------|------------\n"
     for pair_number in range(1, len(MAJOR_COLORS) * len(MINOR_COLORS) + 1):
         major_color, minor_color = get_color_from_pair_number(pair_number)
-        manual += f'{pair_number:11} | {major_color:12} | {minor_color}\n'
+        manual += f'{pair_number:11} | {major_color:11} | {minor_color}\n'
     return manual
