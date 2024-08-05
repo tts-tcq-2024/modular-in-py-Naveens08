6e1a13753cbde33d7b9a4192c7269ee6c38fdbdd
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-08-05 09:23:52 +0530

    Update colorCodeTests.py

--- colorCodeTests.py
+++ colorCodeTests.py
@@ -12,35 +12,37 @@ def test_pair_to_number(major_color, minor_color, expected_pair_number):
   pair_number = get_pair_number_from_color(major_color, minor_color)
   assert(pair_number == expected_pair_number)
 
-def test_print_color_coding_manual():
-  manual = "Color Coding Reference Manual
-Pair Number | Major Color | Minor Color
-------------|-------------|------------
-          1 | White        | Blue
-          2 | White        | Orange
-          3 | White        | Green
-          4 | White        | Brown
-          5 | White        | Slate
-          6 | Red          | Blue
-          7 | Red          | Orange
-          8 | Red          | Green
-          9 | Red          | Brown
-         10 | Red          | Slate
-         11 | Black        | Blue
-         12 | Black        | Orange
-         13 | Black        | Green
-         14 | Black        | Brown
-         15 | Black        | Slate
-         16 | Yellow       | Blue
-         17 | Yellow       | Orange
-         18 | Yellow       | Green
-         19 | Yellow       | Brown
-         20 | Yellow       | Slate
-         21 | Violet       | Blue
-         22 | Violet       | Orange
-         23 | Violet       | Green
-         24 | Violet       | Brown
-         25 | Violet       | Slate
-"  
-  assert(manual == get_color_coding_ref_manual())
+def test_get_color_coding_ref_manual():
+    expected_manual = (
+        "Color Coding Reference Manual\n"
+        "Pair Number | Major Color | Minor Color\n"
+        "------------|-------------|------------\n"
+        "          1 | White       | Blue\n"
+        "          2 | White       | Orange\n"
+        "          3 | White       | Green\n"
+        "          4 | White       | Brown\n"
+        "          5 | White       | Slate\n"
+        "          6 | Red         | Blue\n"
+        "          7 | Red         | Orange\n"
+        "          8 | Red         | Green\n"
+        "          9 | Red         | Brown\n"
+        "         10 | Red         | Slate\n"
+        "         11 | Black       | Blue\n"
+        "         12 | Black       | Orange\n"
+        "         13 | Black       | Green\n"
+        "         14 | Black       | Brown\n"
+        "         15 | Black       | Slate\n"
+        "         16 | Yellow      | Blue\n"
+        "         17 | Yellow      | Orange\n"
+        "         18 | Yellow      | Green\n"
+        "         19 | Yellow      | Brown\n"
+        "         20 | Yellow      | Slate\n"
+        "         21 | Violet      | Blue\n"
+        "         22 | Violet      | Orange\n"
+        "         23 | Violet      | Green\n"
+        "         24 | Violet      | Brown\n"
+        "         25 | Violet      | Slate\n"
+    )
     
+    actual_manual = get_color_coding_ref_manual()
+    assert (actual_manual == expected_manual)
